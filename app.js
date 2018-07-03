afa47af2db74f479a94328fd3a1f3c3949328906
EC2 Default User <ec2-user@ip-172-31-92-85.ec2.internal>
2018-07-03 08:08:28 +0000

    nav bar works

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/", function(req, res){
     res.render("home")
 });
 
+app.get("/scales", function(req, res){
+    res.render("scales")
+});
+
+app.get("/activities", function(req, res){
+    res.render("activities")
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Has Started")
 })
