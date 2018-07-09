9de572939165db5ab99e86f2ac16893684b3ba49
EC2 Default User <ec2-user@ip-172-31-92-85.ec2.internal>
2018-07-09 08:08:50 +0000

    url changed

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var activityRoutes      = require("./routes/activityRoutes/activitiesHome"),
     authRoutes          = require("./routes/auth")
     
 // console.log(process.env.DATABASEURL)
-mongoose.connect("mongodb://localhost:27017/heroicDB", { useNewUrlParser: true })
-// mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true })
+// mongoose.connect("mongodb://localhost:27017/heroicDB", { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true })
 // mongoose.connect("mongodb://av:password1@ds127851.mlab.com:27851/heroic");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
